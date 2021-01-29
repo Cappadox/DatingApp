beb663c1bb001af1a2a12db61d621e1fee50e22b
cappadox <cerits@itu.edu.tr>
2021-01-29 22:23:08 +0300

    section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -22,7 +22,7 @@ namespace API
     public Startup(IConfiguration config)
     {
       _config = config;
-      
+
     }
 
     // This method gets called by the runtime. Use this method to add services to the container.
@@ -37,7 +37,7 @@ namespace API
       {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
       });
-
+      services.AddCors();
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +53,8 @@ namespace API
       app.UseHttpsRedirection();
 
       app.UseRouting();
+
+      app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
       app.UseAuthorization();
 
